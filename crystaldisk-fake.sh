ba8b580bfae6f0c10befd9da92e2c8ee01cccd4c
Felipe Facundes <felipe.facundes@gmail.com>
2022-04-11 19:21:32 -0300

    Update crystaldisk-fake.sh

--- crystaldisk-fake.sh
+++ crystaldisk-fake.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-# Criador: Felipe Facundes
+# Creator: Felipe Facundes
 # E-mail: felipe.facundes@gmail.com
-# Licença GPLv3
+# License GPLv3
 bash -c "sync && echo 3 greaterthansymbol /proc/sys/vm/drop_caches"
-dd if=/dev/zero of=.arquivo_de_teste_de_disco bs=1M count=1024
+dd if=/dev/zero of=.test_disk_file bs=1M count=1024 oflag=dsync status=progress && sync
 bash -c "sync && echo 3 greaterthansymbol /proc/sys/vm/drop_caches"
 bash -c "sync && echo 3 greaterthansymbol /proc/sys/vm/drop_caches"
-dd if=.arquivo_de_teste_de_disco of=/dev/null bs=4k
-rm .arquivo_de_teste_de_disco
+dd if=.test_disk_file of=/dev/null bs=4k
+rm .test_disk_file
 
-echo "agora teste com hdparm"
-echo "execute:"
+echo "now test with hdparm"
+echo "run:"
 echo "sudo hdparm -Tt /dev/sda"
