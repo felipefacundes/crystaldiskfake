12c4b69f6573c1002d3fe0e1dd89b5c7d56cea51
Felipe Facundes <felipe.facundes@gmail.com>
2022-05-03 18:54:46 -0300

    Update crystaldisk-fake.sh

--- crystaldisk-fake.sh
+++ crystaldisk-fake.sh
@@ -3,21 +3,34 @@
 # E-mail: felipe.facundes@gmail.com
 # License GPLv3
 bash -c "sync && echo 3 | sudo tee /proc/sys/vm/drop_caches" > /dev/null
-tput setaf 2; echo "Write Test:"
+test_disk_file=.test_disk_file_`date +"%d-%m-%Y_%H:%M:%S"`
+echo
+tput setaf 2; echo "Writing Test (Method 01):"
 tput sgr0
 echo
-dd if=/dev/zero of=.test_disk_file bs=8M count=512 oflag=dsync status=progress && sync
+dd if=/dev/zero of="${test_disk_file}" bs=64M count=16 oflag=dsync status=progress
 echo
-bash -c "sync && echo 3 | sudo tee /proc/sys/vm/drop_caches" > /dev/null
+tput setaf 2; echo "Writing Test (Method 02):"
+tput sgr0
+echo
+rm "${test_disk_file}"
+dd if=/dev/zero of="${test_disk_file}" bs=64M count=16 oflag=direct status=progress
+echo
+tput setaf 2; echo "Copy Test:"
+tput sgr0
+echo
+time cp "${test_disk_file}" .test_disk_file_copy
+rm .test_disk_file_copy
 bash -c "sync && echo 3 | sudo tee /proc/sys/vm/drop_caches" > /dev/null
 tput setaf 2; echo "Read Test:"
 tput sgr0
 echo
-dd if=.test_disk_file of=/dev/null bs=8k oflag=dsync status=progress && sync
-rm .test_disk_file
+dd if="${test_disk_file}" of=/dev/null bs=8k oflag=dsync status=progress
+rm "${test_disk_file}"
 echo
 tput setaf 2
 echo "now test with hdparm"
 echo "run:"
+tput setaf 3
 echo "sudo hdparm -Tt /dev/sda"
 tput sgr0
