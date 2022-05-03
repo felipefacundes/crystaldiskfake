7269d412557e55e054f99f3f6fa259b05c3cf296
Felipe Facundes <felipe.facundes@gmail.com>
2022-05-03 15:05:39 -0300

    Update crystaldisk-fake.sh

--- crystaldisk-fake.sh
+++ crystaldisk-fake.sh
@@ -2,13 +2,21 @@
 # Creator: Felipe Facundes
 # E-mail: felipe.facundes@gmail.com
 # License GPLv3
-bash -c "sync && echo 3 greaterthansymbol /proc/sys/vm/drop_caches"
-dd if=/dev/zero of=.test_disk_file bs=1M count=1024 oflag=dsync status=progress && sync
-bash -c "sync && echo 3 greaterthansymbol /proc/sys/vm/drop_caches"
-bash -c "sync && echo 3 greaterthansymbol /proc/sys/vm/drop_caches"
-dd if=.test_disk_file of=/dev/null bs=4k
+bash -c "sync && echo 3 | sudo tee /proc/sys/vm/drop_caches"
+tput setaf 2; echo "Write Test:"
+tput sgr0
+echo
+dd if=/dev/zero of=.test_disk_file bs=8M count=512 oflag=dsync status=progress && sync
+bash -c "sync && echo 3 | sudo tee /proc/sys/vm/drop_caches"
+bash -c "sync && echo 3 | sudo tee /proc/sys/vm/drop_caches"
+tput setaf 2; echo "Read Test:"
+tput sgr0
+echo
+dd if=.test_disk_file of=/dev/null bs=8k oflag=dsync status=progress && sync
 rm .test_disk_file
 
+tput setaf 2
 echo "now test with hdparm"
 echo "run:"
 echo "sudo hdparm -Tt /dev/sda"
+tput sgr0
